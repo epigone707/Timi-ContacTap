506298b94ae1009ed3d9ec0e819b3fc51a40f9d9
Yanfu <yanfu-guo@sjtu.edu.cn>
2021-10-27 23:50:16 -0400

    update home fragment and drawer

--- ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/ui/home/HomeFragment.kt
+++ ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package edu.umich.yanfuguo.contactap.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import edu.umich.yanfuguo.contactap.R
 import edu.umich.yanfuguo.contactap.databinding.FragmentHomeBinding
+import edu.umich.yanfuguo.contactap.ui.ShareActivity
 
 class HomeFragment : Fragment() {
 
@@ -35,11 +39,23 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        val shareButton: ExtendedFloatingActionButton = binding.homeShareFab
+        // When click on the share button on the right bottom corner,
+        // start the ShareActivity
+        shareButton.setOnClickListener{
+            requireActivity().run{
+                startActivity(Intent(this, ShareActivity::class.java))
+                finish()
+            }
+        }
         return root
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
