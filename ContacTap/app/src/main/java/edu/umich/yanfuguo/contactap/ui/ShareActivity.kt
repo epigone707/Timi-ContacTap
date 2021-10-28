eef595e49a31ed4f03d352759b0450801bbda04d
Yanfu <yanfu-guo@sjtu.edu.cn>
2021-10-27 22:46:21 -0400

    initial commit

---
+++ ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/ui/ShareActivity.kt
@@ -0,0 +1,12 @@
+package edu.umich.yanfuguo.contactap.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.umich.yanfuguo.contactap.R
+
+class ShareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share)
+    }
+}
