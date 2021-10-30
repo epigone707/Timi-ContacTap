5f4a369897cb1a9d434e86a01bc6a55d06356638
zyayoung <1328410180@qq.com>
2021-10-30 01:49:38 -0400

    nfc card emu

--- ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/ui/ShareActivity.kt
+++ ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/ui/ShareActivity.kt
@@ -1,33 +1,93 @@
 package edu.umich.yanfuguo.contactap.ui
 
-import android.R
+import android.R.layout.simple_spinner_dropdown_item
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.ColorStateList
+import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import edu.umich.yanfuguo.contactap.KHostApduService
 import edu.umich.yanfuguo.contactap.databinding.ActivityShareBinding
+import edu.umich.yanfuguo.contactap.R
 import edu.umich.yanfuguo.contactap.R.color.share_active
 import edu.umich.yanfuguo.contactap.R.color.share_inactive
-import android.widget.ArrayAdapter
 import edu.umich.yanfuguo.contactap.toast
 
 class ShareActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private var isSharing = false
-    private lateinit var share_view: ActivityShareBinding
+    private lateinit var shareView: ActivityShareBinding
+
+    private var mNfcAdapter: NfcAdapter? = null
+    private lateinit var mTurnNfcDialog: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        share_view = ActivityShareBinding.inflate(layoutInflater)
-        setContentView(share_view.root)
+        shareView = ActivityShareBinding.inflate(layoutInflater)
+        setContentView(shareView.root)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
-        supportActionBar?.setDisplayShowHomeEnabled(true);
+        // add back (left arrow) button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
 
+        // init profile selection Spinner
         val items = arrayOf("Personal", "Business", "EECS 441")
-        val adapter = ArrayAdapter(this, R.layout.simple_spinner_dropdown_item, items)
-        share_view.profileSelector.adapter = adapter
-        share_view.profileSelector.onItemSelectedListener = this;
+        val adapter = ArrayAdapter(this, simple_spinner_dropdown_item, items)
+        shareView.profileSelector.adapter = adapter
+        shareView.profileSelector.onItemSelectedListener = this
+
+        // init NFC
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+    }
+
+    private fun initNFCFunction(): Boolean {
+        return if (checkNFCEnable() && packageManager.hasSystemFeature(PackageManager.FEATURE_NFC_HOST_CARD_EMULATION)) {
+            toggleService(true)
+            true
+        } else {
+            showTurnOnNfcDialog()
+            false
+        }
+    }
+
+    private fun toggleService(enable: Boolean) {
+        val intent = Intent(this@ShareActivity, KHostApduService::class.java)
+        intent.putExtra("ndefMessage", "somedata")
+        if(enable) startService(intent) else stopService(intent)
+    }
+
+    private fun checkNFCEnable(): Boolean {
+        return if (mNfcAdapter == null) {
+            toast(getString(R.string.tv_noNfc))
+            false
+        } else {
+            mNfcAdapter!!.isEnabled
+        }
+    }
+
+    private fun showTurnOnNfcDialog() {
+        mTurnNfcDialog = AlertDialog.Builder(this)
+            .setTitle(getString(R.string.ad_nfcTurnOn_title))
+            .setMessage(getString(R.string.ad_nfcTurnOn_message))
+            .setPositiveButton(
+                getString(R.string.ad_nfcTurnOn_pos)
+            ) { _, _ ->
+                startActivity(Intent(android.provider.Settings.ACTION_NFC_SETTINGS))
+            }.setNegativeButton(getString(R.string.ad_nfcTurnOn_neg)) { _, _ ->
+                onBackPressed()
+            }
+            .create()
+        mTurnNfcDialog.show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isSharing && mNfcAdapter!!.isEnabled) {
+            initNFCFunction()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -46,9 +106,16 @@ class ShareActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     }
 
     fun toggleShare(view: View?){
-        isSharing = !isSharing;
-        share_view.shareButton.backgroundTintList = ColorStateList.valueOf(
-            getColor(if(isSharing) share_active else share_inactive));
-        share_view.shareButton.text = if (isSharing) "SHAREING" else "SHARE";
+        if (!isSharing) {
+            isSharing = initNFCFunction()
+        } else {
+            toggleService(false)
+            isSharing = false
+        }
+
+        // Update UI
+        shareView.shareButton.backgroundTintList = ColorStateList.valueOf(
+            getColor(if(isSharing) share_active else share_inactive))
+        shareView.shareButton.text = if (isSharing) "SHAREING" else "SHARE"
     }
 }
