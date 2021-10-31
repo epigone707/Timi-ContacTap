98febfdd6f12cc1f3b8a91d662805f6247180c1d
Riochenyifu <44956537+Riochenyifu@users.noreply.github.com>
2021-10-31 19:34:02 -0400

    Clear all files

--- ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/Extensions.kt
+++
@@ -1,8 +0,0 @@
-package edu.umich.yanfuguo.contactap
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.toast(message: String, short: Boolean = true) {
-    Toast.makeText(this, message, if (short) Toast.LENGTH_SHORT else Toast.LENGTH_LONG).show()
-}
