5701678aa8e4b079a158ca9d7bcbfedde268e44b
Riochenyifu <yifuch@umich.edu>
2021-11-09 01:13:11 -0500

    Update whole set info list in Contact.kt and ContactInfoActivity.kt

--- ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/model/Contact.kt
+++ ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/model/Contact.kt
@@ -3,13 +3,16 @@ package edu.umich.yanfuguo.contactap.model
 data class Contact (
     var photo: String? = null,
     var name: String = "",
-    var phone: String = "",
-    var email: String = "",
+    var personal_email: String = "",
+    var business_email: String = "",
+    var personal_phone: String = "",
+    var business_phone: String = "",
+    var other_phone: String = "",
+    var bio: String = "",
     var insta: String = "",
     var snap: String = "",
     var twitter: String = "",
     var linkedin: String = "",
-    var other: String = "",
     var hobbies: String = "",
     var otherinfo: String = ""
 )
