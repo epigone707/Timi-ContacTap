cdc926f7d990d1aaba124b6dc107c90266ea4c21
zyayoung <1328410180@qq.com>
2021-11-25 19:03:44 -0500

    notification

--- ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/ReceiveActivity.kt
+++ ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/ReceiveActivity.kt
@@ -1,16 +1,26 @@
 package edu.umich.yanfuguo.contactap
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.gson.Gson
 import edu.umich.yanfuguo.contactap.model.ConnectionStore
 import edu.umich.yanfuguo.contactap.model.Contact
 import edu.umich.yanfuguo.contactap.nfc.NdefMessageParser
+import edu.umich.yanfuguo.contactap.ui.contactList.ContactListActivity
+import edu.umich.yanfuguo.contactap.ui.profileList.ProfileActivity
 
 open class ReceiveActivity : AppCompatActivity() {
+    private val CHANNEL_ID = "0"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +31,38 @@ open class ReceiveActivity : AppCompatActivity() {
                 NdefMessageParser.parse(msg).forEach { record->
                     builder.append(record.str()).append("\n")
                 }
-                toast("Contact received!")
                 ConnectionStore.init(this)
                 ConnectionStore.insert(this, Gson().fromJson(builder.toString(), Contact::class.java))
+
+                val _intent = Intent(this, ContactListActivity::class.java)
+                val pendingIntent = PendingIntent.getActivity(this, 0, _intent, 0)
+                createNotificationChannel()
+                val notification_builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_baseline_person_24)
+                    .setContentTitle("ContacTap")
+                    .setContentText("New contact received!")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                // Issue the notification.
+                with(NotificationManagerCompat.from(this)) {
+                    notify(0, notification_builder.build())
+                }
             }
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, "Contact", importance).apply {
+                description = "Notify on new contact received"
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
