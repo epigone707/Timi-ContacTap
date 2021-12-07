bffa1ea095ea6d92c52dc961f50f861d318f8864
Yanfu <yanfu-guo@sjtu.edu.cn>
2021-12-07 14:00:40 -0500

    show a dialog window to ask user to login

--- ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/ui/HomeFragment.kt
+++ ContacTap/app/src/main/java/edu/umich/yanfuguo/contactap/ui/HomeFragment.kt
@@ -1,20 +1,19 @@
 package edu.umich.yanfuguo.contactap.ui
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.gson.Gson
 import edu.umich.yanfuguo.contactap.databinding.FragmentHomeBinding
-import edu.umich.yanfuguo.contactap.model.MyInfoStore
 import edu.umich.yanfuguo.contactap.model.MyInfoStore.myInfo
-import edu.umich.yanfuguo.contactap.model.ProfileStore
-import org.json.JSONException
-import org.json.JSONObject
 import edu.umich.yanfuguo.contactap.R
 import android.util.Log
+import edu.umich.yanfuguo.contactap.toast
+import edu.umich.yanfuguo.contactap.model.LoginInfo
 import edu.umich.yanfuguo.contactap.model.MyInfoStore.getOverview
 
 
@@ -40,7 +39,38 @@ class HomeFragment : Fragment() {
             startActivity(intent)
         }
 
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(LoginInfo.idToken==null) {
+            activity?.let {
+                val builder = AlertDialog.Builder(it)
+                builder.setMessage(R.string.dialog_text)
+                builder.apply {
+                    setPositiveButton(R.string.dialog_login
+                    ) { dialog, _ ->
+                        // User clicked OK button
+                        val intent = Intent(activity, SignInActivity::class.java)
+                        startActivity(intent)
+                    }
+                    setNegativeButton("No",
+                    ){ dialog, _ ->
+                            dialog.cancel()
+                    }
+                    setTitle("Sorry!")
+                }
+                // Set other dialog properties
+
+                // Create the AlertDialog
+                builder.create()
+            }?.show()
+            Log.d("HomeFragment", "not login. show alert dialog.")
+        }else{
+            Log.d("HomeFragment","already login.")
+        }
     }
 
     override fun onDestroyView() {
